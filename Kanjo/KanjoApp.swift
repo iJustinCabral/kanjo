934fe428a5a5ce9247427b90556231984dc94fff
iJustinCabral <ijustincabral@gmail.com>
2022-12-17 13:24:14 -0500

    Initial Commit

---
+++ Kanjo/KanjoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjoApp.swift
+//  Kanjo
+//
+//  Created by Justin Cabral on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct KanjoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
